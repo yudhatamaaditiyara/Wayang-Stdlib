ac08144c22a618ade40c4ce0ddcc6425e000a249
yudhatamaaditiyara <yudhatamaaditiyara@gmail.com>
2020-11-18 19:53:35 +0700

    docs: fix typo copyright year

--- test/src/StringableInterfaceTest.php
+++ test/src/StringableInterfaceTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 /**
- * Copyright 2020 Yudha Tama Aditiyara
+ * Copyright 2019 Yudha Tama Aditiyara
  * SPDX-License-Identifier: Apache-2.0
  */
 namespace Wayang\Stdlib;
