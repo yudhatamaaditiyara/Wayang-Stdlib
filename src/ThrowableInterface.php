f3f3a327d68f9771b843505dc5b2662f1498d715
yudhatamaaditiyara <yudhatamaaditiyara@gmail.com>
2020-11-18 16:56:57 +0700

    refactor: path

---
+++ src/ThrowableInterface.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright 2020 Yudha Tama Aditiyara
+ * SPDX-License-Identifier: Apache-2.0
+ */
+namespace Wayang\Stdlib;
+
+use Throwable;
+
+interface ThrowableInterface extends Throwable
+{}
