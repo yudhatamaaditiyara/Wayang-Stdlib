04eb03f084242abccfe0b9b348d4db65f04fdeba
yudhatamaaditiyara <yudhatamaaditiyara@gmail.com>
2020-11-18 16:56:21 +0700

    feat: add StringableInterface

---
+++ src/StringableInterface.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright 2020 Yudha Tama Aditiyara
+ * SPDX-License-Identifier: Apache-2.0
+ */
+namespace Wayang\Stdlib;
+
+interface StringableInterface
+{}
